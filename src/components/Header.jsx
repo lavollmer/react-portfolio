39f88f15dd11c5c62429ad33bb3990b0c7a94a44
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-29 09:03:39 -0600

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,16 +23,16 @@ function NavTabs() {
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
         >
-          About
+          About Me
         </Link>
       </li>
       <li className="nav-item">
         <Link
-          to="/Blog"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
+          to="/Portfolio"
+          // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
         >
-          Blog
+          Portfolio
         </Link>
       </li>
       <li className="nav-item">
@@ -42,6 +42,15 @@ function NavTabs() {
           className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Resume"
+          // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
         </Link>
       </li>
     </ul>
